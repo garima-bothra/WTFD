8e4270bba92e77c049030dd8264d31af827be047
garima94921 <gaarimabothra@gmail.com>
2020-06-03 12:23:16 +0530

    Created RecipeStepsViewController

---
+++ WTFD/View/RecipeStepsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeStepsTableViewCell.swift
+//  WTFD
+//
+//  Created by Garima Bothra on 03/06/20.
+//  Copyright © 2020 Garima Bothra. All rights reserved.
+//
+
+import UIKit
+
+class RecipeStepsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
