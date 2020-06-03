b22bc4c903c6a0f7871886be19b031b2126d60d1
garima94921 <gaarimabothra@gmail.com>
2020-06-03 12:36:00 +0530

    RecipeStepsViewController reconfigured

--- WTFD/View/RecipeStepsTableViewCell.swift
+++ WTFD/View/RecipeStepsTableViewCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class RecipeStepsTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var stepLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
