95f565b1168221e0a8e210c265f8e81835fd6f0e
garima94921 <gaarimabothra@gmail.com>
2020-06-03 17:28:41 +0530

    Added option to delete ingredients

--- WTFD/Controller/SearchRecipeViewController.swift
+++ WTFD/Controller/SearchRecipeViewController.swift
@@ -80,4 +80,19 @@ extension SearchRecipeViewController: UITableViewDelegate, UITableViewDataSource
         cell.ingredientLabel.text = ingredients[indexPath.row]
                return cell
     }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+
+        if editingStyle == .delete {
+
+            // remove the item from the data model
+            ingredients.remove(at: indexPath.row)
+
+            // delete the table view row
+            tableView.deleteRows(at: [indexPath], with: .fade)
+
+        } else if editingStyle == .insert {
+            // Not used in our example, but if you were adding a new row, this is where you would do it.
+        }
+    }
 }
